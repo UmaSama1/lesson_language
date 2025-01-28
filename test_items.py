9ecb6e153c7e1749807e76ebcb6dfb3bc2776148
umasama <umidkerimov2@gmail.com>
2025-01-28 22:17:49 +0300

    lesson

--- test_items.py
+++ test_items.py
@@ -1,11 +1,13 @@
 import time
-import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
+links = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+import pytest
 
-
-def test_check_button():
-    browser = webdriver.Chrome()
-    browser.get('https://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/')
-    button = browser.find_element(By.XPATH, "//button[@value='Добавить в корзину']")
-    assert button == button
+def test_find_add_to_basket_button(browser):
+    browser.get(links)
+    time.sleep(5)
+    curr_language = browser.execute_script('return window.navigator.language || window.navigator.userLanguage')
+    if 'fr' in curr_language:
+        time.sleep(30)
+        button = browser.find_element(By.XPATH, "//button[@value='Добавить в корзину']")
+        assert 'Добавить в корзину' == button
