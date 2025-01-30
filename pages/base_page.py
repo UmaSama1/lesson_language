231ea55df5dfc2b97a63d7ebe41bedd7aeeadd5c
umasama <umidkerimov2@gmail.com>
2025-01-30 20:56:50 +0300

    lesson_page_object

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,8 @@
-class Basepage():
+
+class BasePage:
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
 
     def open(self):
         self.browser.get(self.url)
-
-
